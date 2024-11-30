c53830cee75949ea1dd512bf9cf902b78c95ddb1
Abel <n01728632@humber.ca>
2024-11-30 00:38:12 -0500

    first question created

---
+++ script.js
@@ -0,0 +1,9 @@
+let student = {
+    name : "John",
+    age : 20,
+    enrolled : true,
+    courses : ["math","science","english"],
+    displayStudent : function(){
+        console.log("What is your age"+ this.age);
+    }
+};
